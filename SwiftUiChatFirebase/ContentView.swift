103baebb81577b7ce610e6a122c63d4b6c69e1df
rnooxx999 <rnooxx999@gmail.com>
2024-05-23 14:35:29 +0300

    Initial Commit

---
+++ SwiftUiChatFirebase/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUiChatFirebase
+//
+//  Created by Ran Ra on 15/11/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiChatFirebase/SwiftUiChatFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiChatFirebaseApp.swift
+//  SwiftUiChatFirebase
+//
+//  Created by Ran Ra on 15/11/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiChatFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
